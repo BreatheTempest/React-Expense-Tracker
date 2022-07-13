96f6bc962ba9044967e0d6041de00b88bc0342ec
vaclacc <srajer.6@seznam.cz>
2022-07-13 18:57:52 +0200

    navbar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,6 +2,9 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "../../assets/logo.svg";
 import Logout from "../../assets/logout.svg";
+import Settings from "../../assets/settings.svg"
+import Transactions from "../../assets/transaction.svg"
+import Dashboard from "../../assets/dashboard.svg"
 
 import "./Navigation.css";
 
@@ -13,9 +16,9 @@ export default function Navigation() {
         <h2>Maglo.</h2>
       </div>
       <nav className="nav-links">
-        <NavLink to="/">Dashboard</NavLink>
-        <NavLink to="expenses">Expenses</NavLink>
-        <NavLink to="settings">Settings</NavLink>
+        <NavLink to="/"><img src={Dashboard} alt={Dashboard}/> Dashboard</NavLink>
+        <NavLink to="expenses"><img src={Transactions} alt={Transactions}/>Expenses</NavLink>
+        <NavLink to="settings"><img src={Settings} alt={Settings}/>Settings</NavLink>
       </nav>
       <div className="nav-logout">
         <img src={Logout} alt="Logout"/>
